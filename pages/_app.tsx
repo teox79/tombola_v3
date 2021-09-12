65376ee7b3a02c1430be8569a56773e40d1b149e
Matteo <teox1979@gmail.com>
2021-09-12 19:34:04 +0200

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+// pages/_app.tsx
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { FC, useEffect } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import theme from '@styles/theme';
+import { Dashboard } from '../template/Dashboard'
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Dashboard>
+          <Component {...pageProps} />
+        </Dashboard>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
